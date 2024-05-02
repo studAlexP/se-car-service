6f643400f235c32f64e9134e556fde60c06c0841
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-05-02 17:08:12 +0200

    add BookingRequestDto for KafkaListener

---
+++ src/main/java/com/example/dto/BookingRequestDto.java
@@ -0,0 +1,63 @@
+package com.example.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class BookingRequestDto {
+
+    private long cardId;
+    private LocalDate pickupDate;
+    private LocalDate returnDate;
+    private LocalDate bookingDate;
+    private BigDecimal price;
+
+    public BookingRequestDto() {}
+
+    public BookingRequestDto(long cardId, LocalDate pickupDate, LocalDate returnDate, LocalDate bookingDate, BigDecimal price) {
+        this.cardId = cardId;
+        this.pickupDate = pickupDate;
+        this.returnDate = returnDate;
+        this.bookingDate = bookingDate;
+        this.price = price;
+    }
+
+    public LocalDate getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDate bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public long getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(long cardId) {
+        this.cardId = cardId;
+    }
+
+    public LocalDate getPickupDate() {
+        return pickupDate;
+    }
+
+    public void setPickupDate(LocalDate pickupDate) {
+        this.pickupDate = pickupDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
