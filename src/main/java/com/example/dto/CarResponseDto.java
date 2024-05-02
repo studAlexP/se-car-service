e8c2f1bdf379708fc757f2dbb4d954dd38e9e9f3
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-05-02 17:12:55 +0200

    move to another package & remove setter

--- src/main/java/com/example/dto/CarResponseDto.java
+++ src/main/java/com/example/dto/CarResponseDto.java
@@ -1,10 +1,10 @@
-package at.ac.fhcampuswien.secarservice.dto;
+package com.example.dto;
 
 import java.math.BigDecimal;
 
 public class CarResponseDto {
 
-    private long id;
+    private String id;
     private String brand;
     private String model;
     private int numberOfSeats;
@@ -12,7 +12,7 @@ public class CarResponseDto {
 
     public CarResponseDto() {}
 
-    public CarResponseDto(long id, String brand, String model, int numberOfSeats, BigDecimal price) {
+    public CarResponseDto(String id, String brand, String model, int numberOfSeats, BigDecimal price) {
         this.id = id;
         this.brand = brand;
         this.model = model;
@@ -20,43 +20,23 @@ public class CarResponseDto {
         this.price = price;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getBrand() {
         return brand;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
     public String getModel() {
         return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
     }
 
     public int getNumberOfSeats() {
         return numberOfSeats;
     }
 
-    public void setNumberOfSeats(int numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
     }
 }
