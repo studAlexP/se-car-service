791aa829dfc3c87eab2280a32c88e47dfb49c600
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-05-09 19:08:40 +0200

    implement getCarById & getAvailableCars with toCurrency

--- src/main/java/com/example/service/CarService.java
+++ src/main/java/com/example/service/CarService.java
@@ -1,13 +1,16 @@
 package com.example.service;
 
-import com.example.dto.CarRequestDto;
+import com.example.currencyConverter.CurrencyConverterClient;
 import com.example.dto.CarResponseDto;
 import com.example.model.Car;
 import com.example.repository.CarRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,12 +23,41 @@ public class CarService {
     }
 
     @Transactional(readOnly = true)
-    public List<CarResponseDto> findAllAvailableCars(CarRequestDto carRequestDto) {
-        List<Car> availableCars = carRepository.findAvailableCars(carRequestDto.getPickupDate(), carRequestDto.getReturnDate());
+    public List<CarResponseDto> getAvailableCars(LocalDate pickupDate, LocalDate returnDate) {
+        List<Car> availableCars = carRepository.findAvailableCars(pickupDate, returnDate);
 
         return availableCars.stream()
                 .map(this::mapCarToCarResponseDto)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<CarResponseDto> getAvailableCars(LocalDate pickupDate, LocalDate returnDate, String toCurrency) {
+        final CurrencyConverterClient currencyConverter = new CurrencyConverterClient("localhost", 9999);
+        List<Car> availableCars = carRepository.findAvailableCars(pickupDate, returnDate);
+
+        return availableCars.stream()
+                .map(car -> {
+                    CarResponseDto carResponseDto = mapCarToCarResponseDto(car);
+
+                    double originalPrice = car.getPrice().doubleValue();
+                    double convertedPrice = currencyConverter.convertCurrency(originalPrice, "USD", toCurrency);
+                    carResponseDto.setPrice(new BigDecimal(convertedPrice));
+
+                    return carResponseDto;
+                })
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CarResponseDto getCarById(String id) {
+        Optional<Car> car = carRepository.findById(id);
+
+        if (car.isEmpty()) {
+            throw new RuntimeException("Car not found");
+        }
+
+        return mapCarToCarResponseDto(car.get());
     }
 
     private CarResponseDto mapCarToCarResponseDto (Car car) {
